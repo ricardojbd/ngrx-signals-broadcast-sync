30ebdb524471f9d7f11a0ef066690caebd6213d8
RicardoJBarrios <ricardojbarrios@gmail.com>
2024-09-24 23:11:24 +0100

    chore: update tests config

--- apps/playground/playwright.config.ts
+++ apps/playground/playwright.config.ts
@@ -35,7 +35,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] }

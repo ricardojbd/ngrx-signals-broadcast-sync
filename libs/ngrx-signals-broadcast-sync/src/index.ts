13c3a54ab78b80df31889d62961890e1ae1f0542
RicardoJBarrios <ricardojbarrios@gmail.com>
2024-11-08 19:54:32 +0000

    chore: update library config

--- libs/ngrx-signals-broadcast-sync/src/index.ts
+++ libs/ngrx-signals-broadcast-sync/src/index.ts
@@ -1,2 +1,3 @@
-export { Options } from './lib/models';
+export { isBroadcastChannelAvailable } from './lib/is-broadcast-channel-available';
 export { withBroadcastState } from './lib/with-broadcast-state';
+export { withBroadcastRequestState } from './lib/with-broadcast-request-state';
